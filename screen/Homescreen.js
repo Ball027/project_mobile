ef3ad7a1f13e78dff47787730472dfce434a5e07
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-10-23 00:01:15 +0700

    Add files via upload

--- screen/Homescreen.js
+++ screen/Homescreen.js
@@ -1,15 +1,42 @@
 
 import React,{ useRef } from "react";
-import {View,Text,Button,StyleSheet,Animated} from "react-native";
-
-const Homescreen = () => {
-
-  return (
-    <View style={styles.container}>
-        <Text>Hi</Text>
-    </View>
-  );
+import {View,Text,Button,StyleSheet,Animated, FlatList} from "react-native";
+import SportItem from "../components/SportItem";
+import { SPORTS } from "../data/data";
+const Homescreen = ({route,navigation}) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <SportItem
+        name={itemData.item.name}
+        // duration={itemData.item.duration}
+        // complexity={itemData.item.complexity}
+        // affordability={itemData.item.affordability}
+        image={itemData.item.imageurl}
+        onSelectMeal={() => {
+          // เขียนโค้ดเพิ่ม
+          // navigation.navigate("MealDetail",{mealtitle:itemData.item.title,mealsteps:itemData.item.steps})
+        }}
+      />
+    );
   };
+  // const catId = route.params.categoryid;
+  // const availableMeals = useSelector(state => state.meals.filteredMeals)
+
+  // const displayedMeals = availableMeals.filter(
+  //   (meal) => meal.categoryIds.indexOf(catId) >= 0
+  // );
+
+  return(
+    <View style={styles.screen}>
+    <FlatList
+      style={{ width: "100%" }}
+      data={SPORTS}
+      renderItem={renderMealItem}
+    />
+    <Text>Hi</Text>
+  </View>
+  );
+};
   
   const styles = StyleSheet.create({
     container: {
--- screen/Scorescreen.js
+++ screen/Scorescreen.js
@@ -6,6 +6,7 @@ const Scorescreen = () => {
 
   return (
     <SafeAreaView style={styles.container3}>
+    <Text style={styles.head}>Your Score</Text>
     <View style={styles.container}>
       <View style={{...styles.cycle, ...{backgroundColor:"red"}}}></View>
       <View style={{...styles.cycle, ...{backgroundColor:"orange"}}}></View>
@@ -13,11 +14,11 @@ const Scorescreen = () => {
       <View style={{...styles.cycle, ...{backgroundColor:"lime"}}}></View>
       <View style={{...styles.cycle, ...{backgroundColor:"green"}}}></View>
     </View>
+    <Text style={styles.score}>100</Text>
     <View style={styles.container2}>
-      <Text style={styles.score}>100</Text>
+      <Text style={styles.rule}>-10 เมื่อจองแล้วไม่มาตามที่จองไว้</Text>
       <Text style={styles.rule}>-20 เมื่อทำผิดกฎการใช้สนามกีฬา</Text>
-      <Text style={styles.rule}>-10 เมื่อจองแล้วไม่ทำการมาใช้</Text>
-      <Text style={styles.rule}>***คะแนนน้อยกว่า40ไม่สามารถจองได้***</Text>
+      <Text style={styles.rule2}>***คะแนนน้อยกว่า40ไม่สามารถจองได้***</Text>
     </View>
   </SafeAreaView>
   );
@@ -25,11 +26,13 @@ const Scorescreen = () => {
   
   const styles = StyleSheet.create({
     container: {
+      // flex:0.5,
       flexDirection:"row",
       justifyContent: "center",
   
     },
     container2: {
+      flex:3,
       justifyContent: "center",
       alignItems: "center",
     },
@@ -38,26 +41,39 @@ const Scorescreen = () => {
       justifyContent: "center",
       alignItems:"center",
     },
+    head:{
+      flex:1.5,
+      fontSize:48,
+      color:"orange",
+      fontWeight:"bold"
+    },
     score:{
-      fontSize:36,
+      fontSize:56,
       fontWeight:"bold",
     },
     rule:{
       fontSize:20,
       color:"red"
     },
+    rule2:{
+      fontSize:20,
+      color:"red",
+      fontWeight:"bold",
+    },
     cycle:{
       borderRadius: 100,
-      height:10,
+      width:60,
+      height:60,
       shadowColor: "black",
       shadowOpacity: 0.26,
       shadowOffset: { width: 0, height: 2 },
       shadowRadius: 10,
       elevation: 3,
       padding: 15,
+      margin:5,
       justifyContent: "flex-end",
       alignItems: "flex-end",
-    }
+    },
   });
   
   export default Scorescreen;
