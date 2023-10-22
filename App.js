0d009389f6b7c86f9ebb7d9ccc4698fed61511d9
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-10-23 00:01:24 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MyNavigator from "./navigation/Mynavigation";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MyNavigator from "./navigation/MyNavigator";
+import { createStore, combineReducers } from "redux";
+import mealsReducer from "./store/reducers/mealsReducer";
+import { Provider } from "react-redux";
+import MyNavigator2 from "./navigation/Mynavigation2";
+
+// import คอมโพเนนต์ที่จำเป็น
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+  })
+
+const store = createStore(rootReducer);
 
 export default function App() {
-  return <MyNavigator />;
+  // เพิ่มโค้ดส่วนนี้ เพื่อจัดการ Stack Navigation
+  // return <Provider store={store}><MyNavigator/></Provider>;
+  return(<MyNavigator2/>);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "blue",
     alignItems: "center",
     justifyContent: "center",
   },
