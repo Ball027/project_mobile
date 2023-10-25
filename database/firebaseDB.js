8f53fe6c09b91780ce9e4c12fad15ab79f25dabb
Natchanon Ruengsrinaraporn <88502240+Ball027@users.noreply.github.com>
2023-10-25 20:13:55 +0700

    Add files via upload

---
+++ database/firebaseDB.js
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/compat";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDocNqjk4yFnWjIxCOt1eMji0-kHpdwkKc",
+    authDomain: "mobile-608b7.firebaseapp.com",
+    projectId: "mobile-608b7",
+    storageBucket: "mobile-608b7.appspot.com",
+    messagingSenderId: "1075709907465",
+    appId: "1:1075709907465:web:b7b3527d647728916c9ed8",
+    measurementId: "G-FTQZM98Y8B"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
